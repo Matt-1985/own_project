36b1ca2a7611e3fccde3f64e080c67fd5f8bfe0a
Matt-1985 <blatsche@web.de>
2020-10-24 19:48:45 +0200

    Add first styling

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,6 +1,5 @@
 import "./quotes.css";
 import { createElement } from "../utils/elements";
-<<<<<<< HEAD
 
 function Quotes({ quote }) {
   const quoteText = createElement("div", {
@@ -9,25 +8,6 @@ function Quotes({ quote }) {
   });
 
   return quoteText;
-=======
-import imgSrc from "../assets/tv.png";
-
-function Quotes() {
-  const quoteDisplay = createElement("img", {
-    className: "quote__display",
-    src: imgSrc,
-  });
-  const quoteText = createElement("section", {
-    className: "quote__text",
-    // innerText: quote,
-  });
-  const quoteContainer = createElement("div", {
-    className: "quote__container",
-    children: [quoteDisplay, quoteText],
-  });
-
-  return quoteContainer;
->>>>>>> master
 }
 
 export default Quotes;
