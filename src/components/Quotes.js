079667cbea6dc442fa1cbc81255dc6133547dd34
Matt-1985 <blatsche@web.de>
2020-10-26 17:20:05 +0100

    Debugging Vercel error

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,7 +1,6 @@
 import "./quotes.css";
 import { createElement } from "../utils/elements";
 
-
 function Quotes({ quote }) {
   const quoteText = createElement("div", {
     className: "quote__text",
@@ -9,25 +8,6 @@ function Quotes({ quote }) {
   });
 
   return quoteText;
-
-import imgSrc from "../assets/tv.png";
-
-function Quotes() {
-  const quoteDisplay = createElement("img", {
-    className: "quote__display",
-    src: imgSrc,
-  });
-  const quoteText = createElement("section", {
-    className: "quote__text",
-    // innerText: quote,
-  });
-  const quoteContainer = createElement("div", {
-    className: "quote__container",
-    children: [quoteDisplay, quoteText],
-  });
-
-  return quoteContainer;
-
 }
 
 export default Quotes;
